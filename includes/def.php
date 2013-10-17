833719abd920d3a294eba29a5ecb1ecdf8c97031
Andrew Nickerson <nickersondesign@gmail.com>
2013-10-17 13:51:33 -0700

    first Commit

---
+++ includes/def.php
@@ -0,0 +1,24 @@
+<?php
+
+define('HOST', 'localhost');
+define('USER', 'root');
+define('PASSWORD', '');
+define('DATABASE', 'database-name');
+
+define('DIRROOT', dirname(__FILE__));
+
+$conn = new mysqli(HOST, USER, PASSWORD, DATABASE);
+if ($conn->connect_errno) {
+    echo "Failed to connect to MySQL: (" . $conn->connect_errno . ") " . $conn->connect_error;
+}
+
+require 'auth.php';
+
+//On whatevr page you'd like password Protected just pass the $conn variable to the auth class, like so:
+$auth = new auth($conn);
+
+
+	
+
+
+
